c3174c9a84aa613e35ffb897eb371ba4be0b636b
peterlord45 <peterdinh647@gmail.com>
2025-08-08 15:07:25 -0400

    Pushing Java_Calculator project

---
+++ src/Main.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+// This project will enhance our skills with enhanced switches!!!
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        double num1;
+        double num2;
+        char operator;
+        double result =0;
+        boolean validOperation = true;
+
+
+        System.out.print("Enter the first number: ");
+        num1 = scanner.nextDouble();
+
+        System.out.print("Enter an operator (+, -, *, /, ^): ");
+        operator = scanner.next().charAt(0);
+
+        System.out.print("Enter the second number: ");
+        num2 = scanner.nextDouble();
+
+        switch(operator){
+            case '+' -> result = num1 + num2;
+            case '-' -> result = num1 - num2;
+            case '*' -> result = num1 * num2;
+            case '/' -> {
+                if(num2 == 0){
+                    System.out.println("cannot divide by zero");
+                    validOperation = false; //will not continue to display result
+                }
+                else{
+                    result = num1 / num2;
+                }
+            }
+            case '^' -> result = Math.pow(num1, num2);
+            default -> {
+                System.out.println("Invalid operator");
+                validOperation = false; //will not continue to display result
+            }
+        }
+        // will only print the result if our validOperation is true
+        if(validOperation){
+            System.out.println(result);
+        }
+
+        scanner.close();
+
+    }
+}
